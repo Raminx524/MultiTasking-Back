7abde518d49d4be806b408d261a296797bd816d1
Raminx524 <Raminx524@Gmail.com>
2024-07-07 12:23:53 +0300

    small fixes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/auth.controller");
+const User = require("../models/user.model");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const user = await User.findById(userId).exec();
+  const { password, ...userWithoutPassword } = user._doc;
+
+  // res.json({ message: `You are accessing protected route ${req.userId}` });
+  res.status(200).json(userWithoutPassword);
+});
 
 module.exports = router;
